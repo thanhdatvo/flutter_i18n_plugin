110392df8cc2bdf603885d99a76656b88a8c7c28
datvt9312 <datvt9312@gmail.com>
2018-09-24 16:29:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_i18n_plugin/constant.dart' show languages;
+//import 'package:flutter_i18n_plugin/localizations.dart'
+//    show MyLocalizations, MyLocalizationsDelegate;
+import 'package:flutter_i18n_plugin/generated/i18n.dart';
+
+void main() async {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  String _locale = 'en';
+  onChangeLanguage() {
+    if (_locale == 'en') {
+      setState(() {
+        _locale = 'fr';
+      });
+    } else {
+      setState(() {
+        _locale = 'en';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        locale: Locale(_locale, ""),
+        localizationsDelegates: [S.delegate],
+        supportedLocales: S.delegate.supportedLocales,
+        localeResolutionCallback:
+            S.delegate.resolution(fallback: new Locale("en", "")),
+        home: AppBody(this.onChangeLanguage));
+  }
+}
+
+class AppBody extends StatelessWidget {
+  final VoidCallback onChangeLanguage;
+  AppBody(this.onChangeLanguage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(S.of(context).hello),
+        ),
+        body: new Center(
+          child: new Text(S.of(context).greetTo('Nina')),
+        ),
+        floatingActionButton: new FloatingActionButton(
+            child: new Icon(Icons.language), onPressed: onChangeLanguage));
+  }
+}
